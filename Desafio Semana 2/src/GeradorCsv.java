713a93546765273bfec2d2394acdcfca64348b58
lucarauj <lukas.martins193@gmail.com>
2023-06-16 19:24:34 -0300

    Update cadastro.csv

--- Desafio Semana 2/src/GeradorCsv.java
+++ Desafio Semana 2/src/GeradorCsv.java
@@ -8,7 +8,7 @@ public class GeradorCsv {
 
     public void gerarCsv(List<StringBuilder> cadastroList) {
         try {
-            File cadastro = new File("caminho do arquivo","cadastro.csv");
+            File cadastro = new File("C:\\Users\\luc_c\\Desktop\\Diversos\\Programação\\MJV\\Java\\mjv-java-school\\mjv-java-school\\Desafio Semana 2","cadastro.csv");
             FileOutputStream output = new FileOutputStream(cadastro);
 
             StringBuilder cabecalho = new StringBuilder();
